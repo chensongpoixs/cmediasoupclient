cbf464a911fd34e647dea2bd1e25068c70cc81d7
chensong <chensongbj25@gmail.com>
2021-11-09 16:24:16 +0800

    [ 1. Websocket 模块
    WebSocketRequest.js
    
    line: 295
    /*if (this.requestedProtocols.indexOf(acceptedProtocol) === -1) {
                this.reject(500);
                throw new Error('Specified protocol was not requested by the client.');
            }*/
    
    2. WebSocketProto 模块
    
    WebsocketServer.js
    
    line:63
    
    /*if (request.requestedProtocols.indexOf(WS_SUBPROTOCOL) === -1)
    		{
    			logger.warn('_onRequest() | invalid/missing Sec-WebSocket-Protocol [request.requestedProtocols = ' + request.requestedProtocols + ']');
    
    			request.reject(403, 'invalid/missing Sec-WebSocket-Protocol');
    
    			return;
    		}*/
    
    ]

---
+++ build.bat
@@ -0,0 +1,5 @@
+ cmake . Bbuild  -DLIBWEBRTC_INCLUDE_PATH:PATH=D:\Work\cmedia_server\webrtc_google\src  -DLIBWEBRTC_BINARY_PATH:PATH=D:\Work\cmedia_server\webrtc_google\src\out\client\obj
+ 
+ 
+ 
+ pause;
